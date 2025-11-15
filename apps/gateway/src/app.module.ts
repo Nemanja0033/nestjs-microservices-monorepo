baac828e8c8c7aff60598e0b9579b2b2ca975401
Nemanja Antonijevic <antonijevicnemanjaa@gmail.com>
2025-11-15 19:21:38 +0100

    add global validation pipe, remove unused app.service

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { microservicesConfig } from 'config/microservices-config';
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Use Global Validation Pipe to prevent "overposting", and send 400 to the client if client sends unknown fields. 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  
   app.enableCors();
   await app.listen(5000);
 }
