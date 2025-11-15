be16d04152492a5f9bf3e386037a2b00bd370153
Nemanja Antonijevic <antonijevicnemanjaa@gmail.com>
2025-11-15 18:43:08 +0100

    configure microservices, db, and register auth service into gateway module

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { microservicesConfig } from 'config/microservices-config';
 
+// Configuring the gateway module to connect with auth microservice via TCP.
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    ClientsModule.register([
+      {
+        name: "AUTH_SERVICE",
+        transport: Transport.TCP,
+        options: microservicesConfig.authService.options
+      }
+    ])
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
